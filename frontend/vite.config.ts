003a40433a9438e24a53f23e2595960d5a22e530
K-Kizuku <kotani.kizuku@gmail.com>
2024-01-14 17:53:30 +0900

    setting: 🔧 fix config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		tsconfigPaths(),
+		pluginRewriteAll(),
+		splitVendorChunkPlugin(),
+		visualizer(),
+	],
+	envDir: "..",
+	build: {
+		assetsInlineLimit: 4096,
+		cssCodeSplit: true,
+		sourcemap: false,
+		minify: "esbuild",
+		rollupOptions: {
+			output: {
+				chunkFileNames: "assets/js/vendor/[hash].js",
+				entryFileNames: "assets/js/[name].js",
+			},
+		},
+	},
+});
