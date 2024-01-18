a4982830ee22555ba09a404bb93209b7163c7e29
K-Kizuku <kotani.kizuku@gmail.com>
2024-01-18 21:29:03 +0900

    feat: 🍻 add common schema

---
+++ frontend/src/schema/common.ts
@@ -0,0 +1,42 @@
+export type Protocol = {
+	eventType: string;
+	user?: User;
+	room?: Room;
+	chatText?: string;
+	option?: Option;
+	timeNow?: string;
+	win?: Role;
+	users?: User[];
+};
+
+export type User = {
+	id?: string;
+	displayName?: string;
+	icon?: string;
+	isWolf?: boolean;
+	score?: number;
+	word?: string;
+	isParticipant?: string;
+	vote?: {
+		id?: string;
+		displayName?: string;
+	};
+};
+
+export type Room = {
+	roomId?: string;
+	roomOwnerId?: string;
+	voteEnded?: boolean;
+};
+
+export type Option = {
+	turnNum?: string;
+	discussTime?: string;
+	voteTime?: string;
+	participantsNum?: string;
+};
+
+export enum Role {
+	wolf = "wolf",
+	citizen = "citizen",
+}
