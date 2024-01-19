3c1682186dda249e34e5e6508a99c18c97dcf406
simesaba <simesaba1120@gmail.com>
2024-01-19 16:48:13 +0900

    feat: 🍻 Userモデル，Voteモデルの定義

---
+++ backend/models/events.go
@@ -0,0 +1,23 @@
+package models
+
+type Vote struct {
+	ID          string `json:"id,omitempty"`
+	DisplayName string `json:"display_name,omitempty"`
+}
+
+type User struct {
+	ID            string `json:"id,omitempty"`
+	DisplayName   string `json:"display_name,omitempty"`
+	Icon          string `json:"icon,omitempty"`
+	IsWolf        bool   `json:"is_wolf"`
+	Score         uint   `json:"score"`
+	Word          string `json:"word,omitempty"`
+	IsParticipant bool   `json:"is_participant"`
+	Vote          `json:"vote,omitempty"`
+}
+
+type Room struct {
+	RoomID      string `json:"room_id"`
+	RoomOwnerID string `json:"room_owner_id"`
+	VoteEnded   string `json:"vote_ended"`
+}
