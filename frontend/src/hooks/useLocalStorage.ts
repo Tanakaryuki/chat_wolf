b85ab26ad5698b91bad1ef16664926b7862c4b13
K-Kizuku <kotani.kizuku@gmail.com>
2024-01-19 15:36:17 +0900

    feat: 🍻 add utility function

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 export const useLocalStorage = (key: string, initialValue: string) => {
-	const [storedValue, setStoredValue] = useState(() => {
-		try {
-			const item = localStorage.getItem(key);
-			return item ? JSON.parse(item) : initialValue;
-		} catch (error) {
-			console.log(error);
-			return initialValue;
-		}
-	});
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue
+    }
+  })
 
-	useEffect(() => {
-		try {
-			const serializedValue = JSON.stringify(storedValue);
-			localStorage.setItem(key, serializedValue);
-		} catch (error) {
-			console.log(error);
-		}
-	}, [key, storedValue]);
+  useEffect(() => {
+    try {
+      const serializedValue = JSON.stringify(storedValue)
+      localStorage.setItem(key, serializedValue)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [key, storedValue])
 
-	return [storedValue, setStoredValue];
-};
+  return [storedValue, setStoredValue]
+}
--- frontend/src/hooks/useSessionStorage.ts
+++ frontend/src/hooks/useSessionStorage.ts
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 
 export const useSessionStorage = (key: string, initialValue: string) => {
-	const [storedValue, setStoredValue] = useState(() => {
-		try {
-			const item = sessionStorage.getItem(key);
-			return item ? JSON.parse(item) : initialValue;
-		} catch (error) {
-			console.log(error);
-			return initialValue;
-		}
-	});
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = sessionStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.log(error)
+      return initialValue
+    }
+  })
 
-	useEffect(() => {
-		try {
-			const serializedValue = JSON.stringify(storedValue);
-			sessionStorage.setItem(key, serializedValue);
-		} catch (error) {
-			console.log(error);
-		}
-	}, [key, storedValue]);
+  useEffect(() => {
+    try {
+      const serializedValue = JSON.stringify(storedValue)
+      sessionStorage.setItem(key, serializedValue)
+    } catch (error) {
+      console.log(error)
+    }
+  }, [key, storedValue])
 
-	return [storedValue, setStoredValue];
-};
+  return [storedValue, setStoredValue]
+}
