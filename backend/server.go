aefda1a563698da6c3130803c2924f7c5d0be993
simesaba <simesaba1120@gmail.com>
2024-01-19 23:06:35 +0900

    change: 🔨 go-redisのバージョンアップ

--- backend/server.go
+++ backend/server.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/Tanakaryuki/chat_wolf/utils/config"
 
-	"github.com/go-redis/redis/v8"
 	"github.com/labstack/echo/v4"
+	"github.com/redis/go-redis/v9"
 )
 
 var Cache *redis.Client
