da44bc496119350a2fcf995a5aa10bbfb3a1dac8
K-Kizuku <kotani.kizuku@gmail.com>
2024-01-19 15:35:10 +0900

    setting: 🔧 lint vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,29 +1,23 @@
-import react from "@vitejs/plugin-react-swc";
-import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig, splitVendorChunkPlugin } from "vite";
-import pluginRewriteAll from "vite-plugin-rewrite-all";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		tsconfigPaths(),
-		pluginRewriteAll(),
-		splitVendorChunkPlugin(),
-		visualizer(),
-	],
-	envDir: "..",
-	build: {
-		assetsInlineLimit: 4096,
-		cssCodeSplit: true,
-		sourcemap: false,
-		minify: "esbuild",
-		rollupOptions: {
-			output: {
-				chunkFileNames: "assets/js/vendor/[hash].js",
-				entryFileNames: "assets/js/[name].js",
-			},
-		},
-	},
-});
+  plugins: [react(), tsconfigPaths(), pluginRewriteAll(), splitVendorChunkPlugin(), visualizer()],
+  envDir: '..',
+  build: {
+    assetsInlineLimit: 4096,
+    cssCodeSplit: true,
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/vendor/[hash].js',
+        entryFileNames: 'assets/js/[name].js',
+      },
+    },
+  },
+})
