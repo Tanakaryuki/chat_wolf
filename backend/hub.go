8dbb9a3c92ad2d5f015f707ee4783447e5ccc958
simesaba <simesaba1120@gmail.com>
2024-01-20 02:16:57 +0900

    feat: 🍻 websocketの基盤実装とテスト用のwebsocketの追加

--- backend/hub.go
+++ backend/hub.go
@@ -1,7 +1,3 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 // Hub maintains the set of active clients and broadcasts messages to the
@@ -20,7 +16,7 @@ type Hub struct {
 	unregister chan *Client
 }
 
-func newHub() *Hub {
+func NewHub() *Hub {
 	return &Hub{
 		broadcast:  make(chan []byte),
 		register:   make(chan *Client),
