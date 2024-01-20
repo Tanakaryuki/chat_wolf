e183cc84ac5a5ab02d0e5905d09b47f80410b7be
simesaba <simesaba1120@gmail.com>
2024-01-20 22:24:00 +0900

    feat: 🍻 redisへの保存とredisから取得する関数の実装

--- backend/redis/save.go
+++ backend/redis/save.go
@@ -1,1 +1,19 @@
 package redis
+
+import "context"
+
+func Set(roomNum string, data []byte) error {
+	if err := Cache.Set(context.TODO(), roomNum, data, 0).Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Get(roomNum string) ([]byte, error) {
+	data, err := Cache.Get(context.TODO(), roomNum).Result()
+	if err != nil {
+		return nil, err
+	}
+	return []byte(data), nil
+}
