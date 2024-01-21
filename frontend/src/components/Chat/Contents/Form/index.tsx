b80ecbb5cad224e27e914c8955171e605653a7cc
K-Kizuku <kotani.kizuku@gmail.com>
2024-01-21 12:25:25 +0900

    feat: 🍻 add Form send message logic

--- frontend/src/components/Chat/Contents/Form/index.tsx
+++ frontend/src/components/Chat/Contents/Form/index.tsx
@@ -1,8 +1,20 @@
 import { FC, useState } from "react";
 import styles from "./index.module.css";
+import {
+  useGameStatusStore,
+  useNameStore,
+  useRefStore,
+  useUUIDStore,
+} from "../../../../store";
+import { commonSchemaToJSON } from "../../../../schema/dto/common";
+import { EventType, Protocol } from "../../../../schema/common";
 
 export const Form: FC = () => {
   const [message, setMessage] = useState<string>("");
+  const ref = useRefStore((state) => state.socketRef);
+  const id = useUUIDStore((state) => state.uuid);
+  const name = useNameStore((state) => state.name);
+  const roomId = useGameStatusStore((state) => state.meta.roomId);
   return (
     <>
       <div className={styles.container}>
@@ -12,7 +24,27 @@ export const Form: FC = () => {
           onChange={(e) => setMessage(e.target.value)}
           placeholder="入力してね！"
         />
-        <button className={styles.button} type="button">
+        <button
+          className={styles.button}
+          type="button"
+          onClick={() => {
+            const data: Protocol = {
+              eventType: EventType.sendChat,
+              user: {
+                id: id,
+                displayName: name,
+                icon: id,
+                isParticipant: true,
+              },
+              chatText: message,
+              room: {
+                roomId: roomId,
+              },
+            };
+            const dataString = commonSchemaToJSON(data);
+            ref?.current?.send(dataString);
+          }}
+        >
           送信
         </button>
       </div>
